b34963ab0f5ad1da8a95ef8dff61231cd2d346ef
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-08-01 17:50:23 +0530

    bruh

--- lua/modules/treesitter.lua
+++ lua/modules/treesitter.lua
@@ -2,7 +2,7 @@ require'nvim-treesitter.configs'.setup {
 	ensure_installed = "all",
 	sync_install = false,
 	auto_install = true,
-	ignore_install = {},
+	ignore_install = {"phpdoc",},
 	highlight = {
 		enable = true,
 		disable = {},
