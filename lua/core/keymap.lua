1ac931a1965683199d55d9ae64e7c71e19e52cea
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-08-01 17:43:31 +0530

    c supp

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -26,6 +26,10 @@ autocmd("FileType", { pattern = "cpp",
 	callback = function()
 		vim.api.nvim_buf_set_keymap(0,"n","<C-c>",":split<CR>:te g++ -std=c++14 -Wshadow -Wall -o %:t:r % -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG && ./%:t:r<CR>i",opts)
 	end})
+autocmd("FileType", { pattern = "c", 
+	callback = function()
+		vim.api.nvim_buf_set_keymap(0,"n","<C-c>",":split<CR>:te g++ -std=c++14 -Wshadow -Wall -o %:t:r % -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG && ./%:t:r<CR>i",opts)
+	end})
 
 -- autocmd("FileType",	{ pattern = "cpp", command = "nnoremap <buffer> <C-c> :split<CR>:te g++ -std=c++14 -Wshadow -Wall -o %:t:r % -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG && ./%:t:r<CR>i"})
 -- autocmd filetype cpp nnoremap <buffer> <C-c> :split<CR>:te /opt/homebrew/Cellar/gcc/11.2.0_1/bin/aarch64-apple-darwin20-g++-11 -std=c++14 -Wshadow -Wall -o %:t:r % && ./%:t:r<CR>i
