a3794fce627a7adae5633935a65d3eb8aaad60e6
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-08-26 11:02:52 +0530

    started webdev :|"

--- lua/modules/lsp.lua
+++ lua/modules/lsp.lua
@@ -54,11 +54,12 @@ lsp.clangd.setup{
 	flags = lsp_flags,
 	capabilities = capabilities,
 }
---
--- require('lspconfig')['tsserver'].setup{
--- 	on_attach = on_attach,
--- 	flags = lsp_flags,
--- }
+
+lsp.tsserver.setup{
+	on_attach = on_attach,
+	flags = lsp_flags,
+}
+
 -- require('lspconfig')['rust_analyzer'].setup{
 -- 	on_attach = on_attach,
 -- 	flags = lsp_flags,
