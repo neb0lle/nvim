5914b0802daaf7ed2de84d05d000ed66e47448f0
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-10-10 12:48:40 +0530

    bruh

--- lua/modules/rundi.lua
+++ lua/modules/rundi.lua
@@ -2,6 +2,8 @@ local rundi = require('rundi')
 
 rundi.setup({
   autocompile = {
-    cpp = "clang++ -std=c++14 -Wshadow -Wall -o %:t:r % -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG && ./%:t:r",
+    python = "python3 '%'",
+    cpp = "g++ -std=c++14 -Wshadow -Wall -o %:t:r % -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG && ./%:t:r",
+    c = "gcc -Wshadow -Wall -o %:t:r % -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG && ./%:t:r",
   },
 })
