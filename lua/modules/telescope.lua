f4bb3dfead75cb09c83084cb10dd9e16717dac27
NevilleJS <69287021+s3dman@users.noreply.github.com>
2024-01-14 12:16:44 +0530

    change binding tel

--- lua/modules/telescope.lua
+++ lua/modules/telescope.lua
@@ -1,23 +1,24 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 
-telescope.setup{
+telescope.setup({
 	defaults = {
 		vimgrep_arguments = {
-			'rg',
-			'--color=never',
-			'--no-heading',
-			'--with-filename',
-			'--line-number',
-			'--column',
-			'--smart-case',
-			'-u' -- thats the new thing
-			},
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"-u", -- that's the new thing
 		},
-}
+	},
+})
 
-vim.keymap.set('n', '<C-n>', builtin.buffers, {})
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fj', builtin.commands, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<C-n>", builtin.buffers, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fj", ":Telescope<CR>", {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
